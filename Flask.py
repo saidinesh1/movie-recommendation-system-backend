9cfe38496a22a106382da6fde6cfefc9cd018831
Sai Dinesh B <sai@foxsense.io>
2024-02-28 14:53:04 +0530

    added backend for yt scrapping

--- Flask.py
+++ Flask.py
@@ -1,13 +1,12 @@
-# Using flask to make an api
-# import necessary libraries and functions
+
 from flask import Flask, jsonify, request
-
-# creating a Flask app
+from scrapper2 import GetYtVideo
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-# on the terminal type: curl http://127.0.0.1:5000/
-# returns hello world when we use GET.
-# returns the data that we send when we use POST.
+
 @app.route('/', methods = ['GET', 'POST'])
 def home():
     if(request.method == 'GET'):
@@ -16,17 +15,15 @@ def home():
         return jsonify({'data': data})
 
 
-# A simple function to calculate the square of a number
-# the number to be squared is sent in the URL when we use GET
-# on the terminal type: curl http://127.0.0.1:5000 / home / 10
-# this returns 100 (square of 10)
-@app.route('/home/<int:num>', methods = ['GET'])
-def disp(num):
 
-    return jsonify({'data': num**2})
+@app.route('/movie/<string:movie_name>', methods = ['GET'])
+@cross_origin()
+def disp(movie_name):
+    result=GetYtVideo(movie_name)
+    return result
 
 
-# driver function
+
 if __name__ == '__main__':
 
     app.run(debug = True)
--- scrapper2.py
+++ scrapper2.py
@@ -4,13 +4,9 @@ import json
 import re
 
 def GetYtVideo(name):
-    r = requests.get('https://www.youtube.com/results?search_query=tejas+trailer')
+    r = requests.get('https://www.youtube.com/results?search_query={0}+official+trailer'.format(name))
     soup = BeautifulSoup(r.content, 'html.parser')
-
-
     script_tags = soup.find_all('script')
-
-
     ytInitialData_json = None
     for script_tag in script_tags:
         if 'ytInitialData' in script_tag.text:
@@ -19,9 +15,9 @@ def GetYtVideo(name):
                 ytInitialData_json = json.loads(match.group(1))
                 break
 
-    with open('tejas.txt', 'w', encoding='utf-8') as file:
-        json.dump(ytInitialData_json.get('contents'), file, indent=4)
+    with open('lover.txt', 'w', encoding='utf-8') as file:
+        json.dump(ytInitialData_json.get('contents'),file, indent=4)
     if ytInitialData_json:
-        return json.dump(ytInitialData_json.get('contents'), file, indent=4)
+        return ytInitialData_json.get('contents')
     else:
         return "No data"
